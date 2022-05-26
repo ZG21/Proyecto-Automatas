c37c19a715d8bbf5b8a1e8a115fece2d9b8cf334
AlejandroGonzalez0 <alejandro.gonzales@aragoconsulting.com>
2022-05-25 20:19:13 -0500

    Corrección de llamado a carpetas y actualización de json de ejemplo

--- cargaArchivos.js
+++ cargaArchivos.js
@@ -1,15 +1,13 @@
 // Get the form and file field
 let form = document.querySelector('#upload');
 let file = document.querySelector('#file');
-let automatas = [];
-
-
+let automata = {};
 
 // Listen for submit events
 form.addEventListener('submit', handleSubmit);
 
-
-function handleSubmit (event) {
+function handleSubmit(event)
+{
 
 	// Stop the form from reloading the page
 	event.preventDefault();
@@ -17,33 +15,23 @@ function handleSubmit (event) {
 	// If there's no file, do nothing
 	if (!file.value.length) return;
 
-	// Create a new FileReader() object
 	let reader = new FileReader();
 
-	// Setup the callback event to run when the file is read
-	
-    reader.onload = lecturaAutomata;
-    //reader.onload = logFile;
-    
-
-	// Read the file
+	reader.onload = lecturaAutomata;
 	reader.readAsText(file.files[0]);
 }
-function logFile (event) {
-    let str = event.target.result;
-	let json = JSON.parse(str);
-    
-	console.log('string', str);
-	console.log('json', json);
-    
+
+/**
+ * 
+ * @param {*} event 
+ */
+function lecturaAutomata(event)
+{
+	automata = JSON.parse(event.target.result);
+	document.getElementById("txt_inp").innerHTML = JSON.stringify(automata);
 }
 
-function lecturaAutomata(event){
-    
-    let automata=JSON.parse(event.target.result);
-    automatas.push(automata)
-    console.log(automatas,"holaa")
-}
-
-console.log(automatas)
-
+setTimeout(() =>
+{
+	console.log(automata);
+}, 10000);
