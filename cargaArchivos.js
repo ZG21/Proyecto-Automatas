5421bbee745b9951c40f535e0b5f03ebaf295691
AlejandroGonzalez0 <alejandro.gonzales@aragoconsulting.com>
2022-05-30 15:38:52 -0500

    Se logra que la graficación quedé funcional y dinamica después de cargar el automata

--- cargaArchivos.js
+++ cargaArchivos.js
@@ -1,100 +1,88 @@
-// Get the form and file field
-let form = document.querySelector('#upload');
-let file = document.querySelector('#file');
-let automata = {};
+$(document).ready(function ()
+{
+	let automata = {};
+	let _codeSyntax = "graph TD;";
+	let file = document.querySelector('#file');
 
-// Listen for submit events
-form.addEventListener('submit', handleSubmit);
+	$("#inp_uploadFile").click(function (event)
+	{
+		event.preventDefault();
+		if (!file.value.length) return;
 
-//Graph creation
-let state = document.createElement('div');
-state.setAttribute('class','mermaid');
-state.setAttribute('id','graph1');
-state.textContent='graph TD;'
+		let reader = new FileReader();
 
-//AObject
-var myCar = {
-    make: 'Ford',
-    model: 'Mustang',
-    year: 1969
-};
+		reader.onload = lecturaAutomata;
+		reader.readAsText(file.files[0]);
 
-function hola(){
-	state.append(myCar.make,';')
-	state.append(myCar.model)
-	for (let i = 0; i < 7; i++) {
-		
+	});
+
+	/**
+	 * Obtiene el contenido del archivo y empieza los procesos de creación y reverso
+	 * @param {*} event 
+	 */
+	function lecturaAutomata(event)
+	{
+		automata = JSON.parse(event.target.result);
+		document.getElementById("txt_inp").innerHTML = JSON.stringify(automata);
+		if (automata)
+		{
+			creacionAutomata(automata);
+			reversoAutomata(automata);
+		}
 	}
-}
-hola()
 
+	/**
+	 * Creará espacio de trabajo para los graficos
+	 */
+	function createSvg($code)
+	{
+		//Graph creation
+		let graph = document.getElementById("graph1");
 
+		let insert = function ($code)
+		{
+			graph.innerHTML = $code;
+		}
 
-document.body.append(state)
+		mermaid.render("preparedScheme", $code, insert);
+	}
 
+	/**
+	 * 
+	 * @param {*} automata 
+	 */
+	function creacionAutomata(automata)
+	{
+		_codeSyntax = _codeSyntax + "a-->b;"
+		createSvg(_codeSyntax);
+	}
 
+	/**
+	 * Función reverso
+	 * @param {*} automata 
+	 */
+	function reversoAutomata(automata)
+	{
+		let revAut = automata
+		let nuevoInicial = revAut.initial
+		revAut.initial = revAut.final
+		revAut.final = nuevoInicial
+		if (revAut.final.length == 1)
+		{
+			for (let i = 0; i < revAut.transition.length; i++)
+			{
+				let transicion = revAut.transition[i]
+				let nuevoFrom = transicion.from
+				transicion.from = transicion.to
+				transicion.to = nuevoFrom
+				nuevoFrom = ""
+				console.log(typeof (transicion), transicion, "DESPUES")
 
+			}
+		}
 
+		_codeSyntax = _codeSyntax + "c-->d;"
+		createSvg(_codeSyntax);
+	}
 
-console.log(document.body.childNodes);
-$(document).ready(function(){
-	mermaid.initialize({ startOnLoad: true });
 });
-
-function handleSubmit(event){
-
-	// Stop the form from reloading the page
-	event.preventDefault();
-
-	// If there's no file, do nothing
-	if (!file.value.length) return;
-
-	let reader = new FileReader();
-
-	reader.onload = lecturaAutomata;
-	reader.readAsText(file.files[0]);
-}
-
-
-function lecturaAutomata(event){
-	automata = JSON.parse(event.target.result);
-	document.getElementById("txt_inp").innerHTML = JSON.stringify(automata);
-	if (automata) {
-		creacionAutomata(automata)
-		reversoAutomata(automata)
-	}
-}
-
-function creacionAutomata(automata){
-	
-	console.log("hello world", automata)
-
-	let state1 = document.getElementById("graph1")
-	state.append("a --> b;")
-	
-	console.log(state1,"perris")
-	document.body.append(state1)
-
-}
-
-function reversoAutomata(automata){
-	let revAut = automata
-	let nuevoInicial = revAut.initial
-	revAut.initial=revAut.final
-	revAut.final=nuevoInicial
-	if(revAut.final.length == 1){
-		 for (let i = 0; i < revAut.transition.length; i++) {
-			 let transicion = revAut.transition[i]
-			 let nuevoFrom = transicion.from
-			 transicion.from = transicion.to
-			 transicion.to = nuevoFrom
-			 nuevoFrom = ""
-			 console.log(typeof(transicion), transicion, "DESPUES")
-			 
-		 }
-	}
-	
-	
-	
-
-}
